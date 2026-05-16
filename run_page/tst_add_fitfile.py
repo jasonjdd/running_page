7a32b17dd36463e329486680cb8d4b80a5d88ee7
jasonjdd <jason.j.reg@gmail.com>
2026-05-16 20:32:25 +0800

    format

--- run_page/tst_add_fitfile.py
+++ run_page/tst_add_fitfile.py
@@ -1,8 +1,8 @@
-from config import JSON_FILE, SQL_FILE, FIT_FOLDER
+from config import JSON_FILE, SQL_FILE, FIT_FOLDER, GPX_FOLDER
 from utils import make_activities_file
-
 
 # make_activities_file(SQL_FILE, FIT_FOLDER, JSON_FILE, "fit")
 if __name__ == "__main__":
     # 只有当此文件作为主程序运行时，才会执行以下代码
     make_activities_file(SQL_FILE, FIT_FOLDER, JSON_FILE, "fit")
+    # make_activities_file(SQL_FILE, GPX_FOLDER, JSON_FILE, "gpx")
